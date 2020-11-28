e0c381bc34f4f1e50d64462989cf78703d3b74c9
Mike Richards <mrichards@democracy.works>
2020-11-28 11:20:51 -0500

    2019 day 1

--- src/Advent/Y2019/Main.hs
+++ src/Advent/Y2019/Main.hs
@@ -3,12 +3,16 @@ module Advent.Y2019.Main where
 import Advent.Util.Challenge (Challenge(..))
 import qualified Advent.Util.CLI as CLI
 
+import qualified Advent.Y2019.Day01 as Day01
+
 year :: Int
 year = 2019
 
 challengeFns :: Challenge -> Maybe (String -> String)
 challengeFns c =
   case c of
+    (Challenge 1 1) -> Just $ show . Day01.part1
+    (Challenge 1 2) -> Just $ show . Day01.part2
     _ -> Nothing
 
 main :: IO ()
