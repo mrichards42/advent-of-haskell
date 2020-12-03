b235bb054f903d515bf27bf7ccb6601f414d8a9d
Mike Richards <mrichards@democracy.works>
2020-12-03 08:51:01 -0500

    2020 day 3

--- src/Advent/Y2020/Challenges.hs
+++ src/Advent/Y2020/Challenges.hs
@@ -6,6 +6,7 @@ import qualified Data.Map as Map
 -- Days
 import qualified Advent.Y2020.Day01 as Day01
 import qualified Advent.Y2020.Day02 as Day02
+import qualified Advent.Y2020.Day03 as Day03
 
 challenges :: ChallengeMap
 challenges =
@@ -13,4 +14,6 @@ challenges =
                           , (Challenge 2020 1 2, show . Day01.part2)
                           , (Challenge 2020 2 1, show . Day02.part1)
                           , (Challenge 2020 2 2, show . Day02.part2)
+                          , (Challenge 2020 3 1, show . Day03.part1)
+                          , (Challenge 2020 3 2, show . Day03.part2)
                           ]
---
+++ src/Advent/Y2020/Day03.hs
@@ -0,0 +1,155 @@
+{- Toboggan Trajectory -}
+module Advent.Y2020.Day03 (part1, part2) where
+
+import Advent.Util.Parsing (Parser, parseOrError)
+
+import Data.Vector (Vector)
+import qualified Data.Vector as V
+import qualified Text.Megaparsec as M
+import qualified Text.Megaparsec.Char as M
+
+{- Part 1
+
+With the toboggan login problems resolved, you set off toward the airport.
+While travel by toboggan might be easy, it's certainly not safe: there's very
+minimal steering and the area is covered in trees. You'll need to see which
+angles will take you near the fewest trees.
+
+Due to the local geology, trees in this area only grow on exact integer
+coordinates in a grid. You make a map (your puzzle input) of the open squares
+(.) and trees (#) you can see. For example:
+
+..##.......
+#...#...#..
+.#....#..#.
+..#.#...#.#
+.#...##..#.
+..#.##.....
+.#.#.#....#
+.#........#
+#.##...#...
+#...##....#
+.#..#...#.#
+
+These aren't the only trees, though; due to something you read about once
+involving arboreal genetics and biome stability, the same pattern repeats to
+the right many times:
+
+..##.........##.........##.........##.........##.........##.......  --->
+#...#...#..#...#...#..#...#...#..#...#...#..#...#...#..#...#...#..
+.#....#..#..#....#..#..#....#..#..#....#..#..#....#..#..#....#..#.
+..#.#...#.#..#.#...#.#..#.#...#.#..#.#...#.#..#.#...#.#..#.#...#.#
+.#...##..#..#...##..#..#...##..#..#...##..#..#...##..#..#...##..#.
+..#.##.......#.##.......#.##.......#.##.......#.##.......#.##.....  --->
+.#.#.#....#.#.#.#....#.#.#.#....#.#.#.#....#.#.#.#....#.#.#.#....#
+.#........#.#........#.#........#.#........#.#........#.#........#
+#.##...#...#.##...#...#.##...#...#.##...#...#.##...#...#.##...#...
+#...##....##...##....##...##....##...##....##...##....##...##....#
+.#..#...#.#.#..#...#.#.#..#...#.#.#..#...#.#.#..#...#.#.#..#...#.#  --->
+
+You start on the open square (.) in the top-left corner and need to reach the
+bottom (below the bottom-most row on your map).
+
+The toboggan can only follow a few specific slopes (you opted for a cheaper
+model that prefers rational numbers); start by counting all the trees you would
+encounter for the slope right 3, down 1:
+
+From your starting position at the top-left, check the position that is right 3
+and down 1. Then, check the position that is right 3 and down 1 from there, and
+so on until you go past the bottom of the map.
+
+The locations you'd check in the above example are marked here with O where
+there was an open square and X where there was a tree:
+
+..##.........##.........##.........##.........##.........##.......  --->
+#..O#...#..#...#...#..#...#...#..#...#...#..#...#...#..#...#...#..
+.#....X..#..#....#..#..#....#..#..#....#..#..#....#..#..#....#..#.
+..#.#...#O#..#.#...#.#..#.#...#.#..#.#...#.#..#.#...#.#..#.#...#.#
+.#...##..#..X...##..#..#...##..#..#...##..#..#...##..#..#...##..#.
+..#.##.......#.X#.......#.##.......#.##.......#.##.......#.##.....  --->
+.#.#.#....#.#.#.#.O..#.#.#.#....#.#.#.#....#.#.#.#....#.#.#.#....#
+.#........#.#........X.#........#.#........#.#........#.#........#
+#.##...#...#.##...#...#.X#...#...#.##...#...#.##...#...#.##...#...
+#...##....##...##....##...#X....##...##....##...##....##...##....#
+.#..#...#.#.#..#...#.#.#..#...X.#.#..#...#.#.#..#...#.#.#..#...#.#  --->
+
+In this example, traversing the map using this slope would cause you to
+encounter 7 trees.
+
+Starting at the top-left corner of your map and following a slope of right 3
+and down 1, how many trees would you encounter?
+
+-}
+
+data Cell = Tree | Open
+  deriving (Show, Eq)
+
+type Row = Vector Cell
+type Field = [Row]
+
+pRow :: Parser Row
+pRow = do
+  row <- M.some (pTree M.<|> pOpen)
+  _ <- M.optional M.newline
+  return $ V.fromList row
+  where pTree = M.char '#' >> return Tree
+        pOpen = M.char '.' >> return Open
+
+parseInput :: String -> Field
+parseInput = parseOrError (M.some pRow)
+
+cellAt :: Row -> Int -> Cell
+cellAt row pos =
+  let pos' = pos `mod` V.length row
+  in row V.! pos'
+
+treesInPath :: Int -> (Int, Int) -> Field -> Int
+treesInPath _ _ [] = 0
+treesInPath pos (dx, dy) field@(row:_) =
+  let n = case cellAt row pos of
+            Tree -> 1
+            Open -> 0
+  in n + treesInPath (pos + dx) (dx, dy) (drop dy field)
+
+-- $setup
+-- >>> example = "..##.......\n#...#...#..\n.#....#..#.\n..#.#...#.#\n.#...##..#.\n..#.##.....\n.#.#.#....#\n.#........#\n#.##...#...\n#...##....#\n.#..#...#.#\n"
+
+-- | >>> part1 example
+-- 7
+-- >>> readFile "input/2020/day03.txt" >>= return . part1
+-- 171
+part1 :: String -> Int
+part1 = treesInPath 0 (3, 1) . parseInput
+
+
+{- Part 2
+
+Time to check the rest of the slopes - you need to minimize the probability of
+a sudden arboreal stop, after all.
+
+Determine the number of trees you would encounter if, for each of the following
+slopes, you start at the top-left corner and traverse the map all the way to
+the bottom:
+
+    Right 1, down 1.
+    Right 3, down 1. (This is the slope you already checked.)
+    Right 5, down 1.
+    Right 7, down 1.
+    Right 1, down 2.
+
+In the above example, these slopes would find 2, 7, 3, 4, and 2 tree(s)
+respectively; multiplied together, these produce the answer 336.
+
+What do you get if you multiply together the number of trees encountered on
+each of the listed slopes?
+
+-}
+
+-- | >>> part2 example
+-- 336
+-- >>> readFile "input/2020/day03.txt" >>= return . part2
+-- 1206576000
+part2 :: String -> Int
+part2 input = product $ map (\slope -> treesInPath 0 slope field) slopes
+  where field = parseInput input
+        slopes = [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)]
