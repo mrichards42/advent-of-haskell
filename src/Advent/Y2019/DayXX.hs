614fe1a4f5f286cb2cdb382ab4ddb3dded018dd7
Mike Richards <mrichards@democracy.works>
2020-12-02 21:50:05 -0500

    Add day templates

---
+++ src/Advent/Y2019/DayXX.hs
@@ -0,0 +1,21 @@
+{- Title -}
+module Advent.Y2019.DayXX (part1, part2) where
+
+{- Part 1
+
+-}
+
+-- | >>> readFile "input/2019/dayXX.txt" >>= return . part1
+-- undefined
+part1 :: String -> Int
+part1 = undefined
+
+
+{- Part 2
+
+-}
+
+-- | >>> readFile "input/2019/dayXX.txt" >>= return . part2
+-- undefined
+part2 :: String -> Int
+part2 = undefined
---
+++ src/Advent/Y2020/DayXX.hs
@@ -0,0 +1,21 @@
+{- Title -}
+module Advent.Y2020.DayXX (part1, part2) where
+
+{- Part 1
+
+-}
+
+-- | >>> readFile "input/2020/dayXX.txt" >>= return . part1
+-- undefined
+part1 :: String -> Int
+part1 = undefined
+
+
+{- Part 2
+
+-}
+
+-- | >>> readFile "input/2020/dayXX.txt" >>= return . part2
+-- undefined
+part2 :: String -> Int
+part2 = undefined
